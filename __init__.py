25a8807b1f7b0698c11b071ed7bdd636500de86d
Sofiane KHEDIM <55292902+SofianeKhedim@users.noreply.github.com>
2025-06-04 16:04:12 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -29,4 +29,5 @@ def somme(valeur1, valeur2):
 
                                                                                                                
 if __name__ == "__main__":
-  app.run(debug=True)
+  app.run(debug=True, host='0.0.0.0', port=5000)
+  #app.run(debug=True)
