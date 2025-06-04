7d701cc728fcff9afbbe782b97cf14f284c3e91f
Lydiahm <109420511+Lydiahm@users.noreply.github.com>
2025-06-04 16:40:32 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -27,9 +27,9 @@ def somme(valeur1, valeur2):
 
 
 
-                                                                                                               
 if __name__ == "__main__":
-  app.run(debug=True)
+  app.run(host="0.0.0.0", port=5000, debug=True)
+
 
 
 
