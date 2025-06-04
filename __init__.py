571f9c9c8f77296ffa907fd2687a6d034750bdc9
Lydiahm <109420511+Lydiahm@users.noreply.github.com>
2025-06-04 13:56:04 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -16,3 +16,5 @@ def exercices():
                                                                                                                
 if __name__ == "__main__":
   app.run(debug=True)
+
+
